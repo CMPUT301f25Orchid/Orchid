81e797da6c84dfb206a4a26b86e3ea3642f07780
AAAA477 <134492910+AAAA477@users.noreply.github.com>
2025-09-24 18:07:35 -0600

    Create Rectangle.java

---
+++ app/src/main/java/com/example/orchid/Rectangle.java
@@ -0,0 +1,12 @@
+package com.example.orchid;
+
+public class Rectangle extends Shape{
+    private Integer width, height;
+
+    public Rectangle(Integer x, Integer y, Integer width, Integer height) {
+
+        this.width = width;
+        this.height = height;
+    }
+
+}
